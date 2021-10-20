60e649ffb0f5f6bff85f9f6cdda997662fe27bff
Samson <samnjoroge6035@gmail.com>
2021-10-20 17:13:13 +0300

    Initial commit

---
+++ app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
@@ -0,0 +1,18 @@
+package com.sriyank.globochat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+}
