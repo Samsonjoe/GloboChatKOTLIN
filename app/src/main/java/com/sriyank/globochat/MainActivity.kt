4e6d259465767e8f733c5ce2edb4468af1937eed
Samson <samnjoroge6035@gmail.com>
2021-11-10 15:41:42 +0300

    Whole app finished

--- app/src/main/java/com/sriyank/globochat/MainActivity.kt
+++ app/src/main/java/com/sriyank/globochat/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.sriyank.globochat
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -27,9 +31,48 @@ class MainActivity : AppCompatActivity() {
 
         // Link ActionBar with NavController
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Step 1: Get reference to the SharedPreferences (XML file)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        // Step 2: Get the value using the 'key'
+        val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time),"")
+        Log.i("MainActivity","Auto Reply Time: $autoReplyTime")
+
+        val publicInfo: Set<String>? = sharedPreferences.getStringSet(getString(R.string.key_public_info),null)
+        Log.i("MainActivity", "Public Info : $publicInfo")
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    //Called only 'after' the preference value has changed
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+
+        if (key == "key_status") {
+            val newStatus = sharedPreferences?.getString(key,"")
+            Toast.makeText(this,"New Status: $newStatus",Toast.LENGTH_SHORT).show()
+        }
+
+        if (key == "key_auto_reply") {
+
+            val autoReply = sharedPreferences?.getBoolean(key,false)
+            if (autoReply!!) {
+                Toast.makeText(this, "Auto Reply: ON",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Auto Reply: OFF",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
     }
 }
--- app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
+++ app/src/main/java/com/sriyank/globochat/SettingsFragment.kt
@@ -1,18 +1,106 @@
 package com.sriyank.globochat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        val dataStore = DataStore()
+        // Enable PreferenceDataStore for entire hierarchy and disables the SharedPreferences
+//        preferenceManager.preferenceDataStore = dataStore
+
+        // Pass the key of the preference object reference to settings.xml
+        val accSettingPref = findPreference<Preference>(getString(R.string.key_account_settings))
+        
+        accSettingPref?.setOnPreferenceClickListener {
+
+            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navController = navHostFragment.navController
+            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+            navController.navigate(action)
+
+            true
+        }
+
+        // Read Preference values in a Fragment
+        // Step 1: Get reference to the SharedPreferences (XML file)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        // Step 2: Get the value using the 'key'
+        val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time),"")
+        Log.i("SettingsFragment","Auto Reply Time: $autoReplyTime")
+
+        val autoDownload = sharedPreferences.getBoolean(getString(R.string.key_auto_download),false)
+        Log.i("SettingsFragment","Auto Download: $autoDownload")
+
+        val statusPref = findPreference<EditTextPreference>(getString(R.string.key_status))
+        //setOnPreferenceChangeListener is implemented before preference is changed
+        statusPref?.setOnPreferenceChangeListener { preference, newValue ->
+
+            val newStatus = newValue as String
+
+            if (newStatus.contains("bad")) {
+
+                Toast.makeText(
+                    context, "Inappropriate status. Please maintain community guidelines",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                false // false reject new value.
+            } else {
+
+                true //true: accept the new value.
+            }
+        }
+
+        val notificationPref = findPreference<SwitchPreferenceCompat>(getString(R.string.key_new_msg_notif))
+        notificationPref?.summaryProvider = Preference.SummaryProvider<SwitchPreferenceCompat> { switchPref ->
+
+            if (switchPref?.isChecked!!)
+                "Status: ON"
+            else
+                "Status: OFF"
+        }
+
+        /*notificationPref?.preferenceDataStore = dataStore
+
+        val isNotifEnabled = dataStore.getBoolean("key_new_msg_notif", false)
+
+         */
     }
 
+    class DataStore : PreferenceDataStore() {
+
+        // Override methods only as per your need.
+        // DO NOT override methods which you don't need to use.
+        // After overriding, remove the super call. (could throw UnsupportedOperationException)
+
+        override fun getBoolean(key: String?, defValue: Boolean): Boolean {
+
+            if (key == "key_new_msg_notif") {
+                // Retrieve value from cloud or local db
+                Log.i("DataStore", "getBoolean executed for $key")
+            }
+
+            return defValue
+        }
+
+        override fun putBoolean(key: String?, value: Boolean) {
+
+            if (key == "key_new_msg_notif") {
+                // Save value to cloud or local db
+                Log.i("DataStore", "putBoolean executed for $key with new value: $value")
+            }
+        }
+    }
 }
